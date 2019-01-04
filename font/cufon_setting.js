ae0efcd078e5a166047dfa8e2d82984afd073aff
Gio Giordano <35657516+mhelzky19@users.noreply.github.com>
2019-01-04 13:40:15 +0800

    Add files via upload

---
+++ font/cufon_setting.js
@@ -0,0 +1,87 @@
+Cufon.replace('#copyrights', {
+    fontFamily: 'PF Din Text Comp Pro Medium',
+    fontWeight: '400',
+    fontSize: '16pt',
+	textShadow: '0px 1px 0px rgba(0,0,0,.5);',
+	color: '#fff'
+});
+
+Cufon.replace('#article p', {
+    fontFamily: 'PF Din Text Comp Pro Medium',
+    fontWeight: '400',
+    fontSize: '18pt',
+	textShadow: '0px 1px 0px rgba(0,0,0,.5);',
+	color: '#fff'
+});
+
+Cufon.replace('#intro-text', {
+    fontFamily: 'PF Din Text Comp Pro Medium',
+    fontWeight: '400',
+    fontSize: '18pt',
+	textShadow: '0px 1px 0px rgba(0,0,0,.5);',
+	color: '#fff'
+});
+
+Cufon.replace('.latest', {
+    fontFamily: 'PF Din Text Comp Pro Medium',
+    fontWeight: '400',
+    fontSize: '28pt',
+	textShadow: '0px 1px 0px rgba(0,0,0,.5);',
+	color: '#bebebe'
+});
+
+Cufon.replace('#list p', {
+    fontFamily: 'PF Din Text Comp Pro Medium',
+    fontWeight: '400',
+    fontSize: '18pt',
+	textShadow: '0px 1px 0px rgba(0,0,0,.5);',
+	color: '#fff'
+});
+
+Cufon.replace('#list span.red', {
+    fontFamily: 'PF Din Text Comp Pro Medium',
+    fontWeight: '400',
+    fontSize: '18pt',
+	textShadow: '0px 1px 0px rgba(0,0,0,.5);',
+	color: '#ad1616'
+});
+
+Cufon.replace('#list span.green', {
+    fontFamily: 'PF Din Text Comp Pro Medium',
+    fontWeight: '400',
+    fontSize: '18pt',
+	textShadow: '0px 1px 0px rgba(0,0,0,.5);',
+	color: '#007c03'
+});
+
+Cufon.replace('#intro-text p span.red', {
+    fontFamily: 'PF Din Text Comp Pro Medium',
+    fontWeight: '400',
+    fontSize: '18pt',
+	textShadow: '0px 1px 0px rgba(0,0,0,.5);',
+	color: '#ad1616'
+});
+
+Cufon.replace('#intro-text p span.green', {
+    fontFamily: 'PF Din Text Comp Pro Medium',
+    fontWeight: '400',
+    fontSize: '18pt',
+	textShadow: '0px 1px 0px rgba(0,0,0,.5);',
+	color: '#007c03'
+});
+
+Cufon.replace('.title, .step-title', {
+    fontFamily: 'PF Din Text Comp Pro Medium',
+    fontWeight: '400',
+    fontSize: '22pt',
+	textShadow: '0px 1px 0px rgba(0,0,0,.5);',
+	color: '#fff'
+});
+
+Cufon.replace('.column-description', {
+    fontFamily: 'PF Din Text Comp Pro Medium',
+    fontWeight: '400',
+    fontSize: '16pt',
+	textShadow: '0px 1px 0px rgba(0,0,0,.5);',
+	color: '#fff'
+});
